59d036faf060a9768d9319be59d219ab36563dba
長谷川凌大 <ryota@ryotaMacBook-Air.local>
2023-11-19 16:35:35 +0900

    repaired edit back

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,6 @@ const confirm_delete = (id) => {
 
 const confirm_edit = (id) => {
     if(!window.confirm(`Task ${id} を編集します．よろしいですか？`)) {
-        document.getElementById("frm").onsubmit = function(){ return false }
-        location.href = `/task/${id}`
+        return false;
     }
 }
